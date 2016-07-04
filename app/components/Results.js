d3586bc6a745bc7717338239c647806700b1418b
Eyrum <1060759539@qq.com>
2016-07-04 14:53:43 +0800

    if applied, this commit will rennder results UI

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,17 +1,64 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
+import {Link} from 'react-router'
+import UserDetails from './UserDetails'
+import UserDetailsWrapper from './UserDetailsWrapper'
+import { transparentBg, space } from '../styles' 
 
-function dump(obj) {
+function StartOver(props) {
   return (
-    <pre>
-      {JSON.stringify(obj, null, ' ')}
-    </pre>
+    <div className="col-sm-12">
+      <Link to='/playerOne'>
+        <button className="btn btn-success btn-lg" style={space}>Start Over</button>
+      </Link>
+    </div>
+  )
+}
+
+function Tie(props) {
+  return (
+    <div className="col-sm-8 col-sm-offset-2 text-center">
+      <h1>It is Tie</h1>Score: {props.score}
+      <StartOver />
+    </div>
   )
 }
 
 function Results(props) {
+  if (props.isLoading === true) {
+    return <p>Loading</p>
+  }
+
+  if (props.scores[0] === props.scores[1]) {
+    return (
+      <Tie score={props.scores[0]} />
+    )
+  }
+
+  const winnerIndex = props.scores[0] > props.scores[1] ? 0 : 1
+  const loserIndex = winnerIndex === 0 ? 1 : 0
+
   return (
-    <div>{dump(props)}</div>
-  )  
+    <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
+      <h1>Results</h1>
+      <div className="col-sm-8 col-sm-offset-2">
+        <UserDetailsWrapper header="winner">
+          <UserDetails score={props.scores[winnerIndex]} data={props.playersInfo[winnerIndex]} />
+        </UserDetailsWrapper>
+        <UserDetailsWrapper header="loser">
+          <UserDetails score={props.scores[loserIndex]} data={props.playersInfo[loserIndex]} />
+        </UserDetailsWrapper>
+      </div>
+      <StartOver />
+    </div>
+  )
+
+
+}
+
+Results.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  playersInfo: PropTypes.array.isRequired,
+  scores: PropTypes.array.isRequired
 }
 
 export default Results;
