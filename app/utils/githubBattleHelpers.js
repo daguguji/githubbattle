e269496fba32f27545057bf124a07881a94c241d
Eyrum <1060759539@qq.com>
2016-07-04 14:21:41 +0800

    if aapplied, this commit will change githubBattleHelpers.js js style to es6

--- app/utils/githubBattleHelpers.js
+++ app/utils/githubBattleHelpers.js
@@ -1,31 +1,20 @@
 import axios from 'axios'
 
-const getUserInfo = (username) => {
-  return axios.get('https://api.github.com/users/' + username)
-}
+const getUserInfo = (username) =>  axios.get('https://api.github.com/users/' + username)
 
-export const getPlayersInfo = (players) => {
-  return axios.all(players.map(username => getUserInfo(username)))
-    .then(function (info) {
-      return info.map(user => user.data)
-    })
-    .catch(err => console.warn('Error in getPlayersInfo', err))
-}
+export const getPlayersInfo = (players) => axios.all(players.map(username => getUserInfo(username)))
+  .then((info) => info.map(user => user.data))
+  .catch(err => console.warn('Error in getPlayersInfo', err))
 
-function getRepos(username) {
-  return axios.get('https://api.github.com/users/' + username + '/repos')
-}
+const getRepos = (username) => axios.get('https://api.github.com/users/' + username + '/repos')
 
-function getTotalStars(repos) {
-  return repos.data.reduce(function (prev, current) {
-    return prev + current.stargazers_count
-  }, 0)
-}
+const getTotalStars = (repos) => repos.data.reduce((prev, current) => prev + current.stargazers_count, 0)
 
-function getPlayersData(player) {
+
+const getPlayersData = (player) => {
   return getRepos(player.login)
     .then(getTotalStars)
-    .then(function (totalStars) {
+    .then((totalStars) => {
       return {
         followers: player.followers,
         totalStars: totalStars
@@ -33,12 +22,7 @@ function getPlayersData(player) {
     })
 }
 
-function calculateScores(players) {
-  return [
-    players[0].followers * 3 + players[0].totalStars,
-    players[1].followers * 3 + players[1].totalStars
-  ]
-}
+const calculateScores = (players) =>  [players[0].followers * 3 + players[0].totalStars, players[1].followers * 3 + players[1].totalStars]
 
 export const battle = (players) => {
   const playerOneData = getPlayersData(players[0])
